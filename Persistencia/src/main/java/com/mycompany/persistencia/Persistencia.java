7b71afb733176b52493beca72f3fa8a5e84cb35f
JLMadero <madero26jose08@gmail.com>
2025-01-15 15:39:45 -0700

    termine avance 1

--- Persistencia/src/main/java/com/mycompany/persistencia/Persistencia.java
+++ Persistencia/src/main/java/com/mycompany/persistencia/Persistencia.java
@@ -15,14 +15,17 @@ import com.mycompany.dto.ComunDTO;
 import com.mycompany.dto.EstadoDTO;
 import com.mycompany.dto.MunicipioDTO;
 import com.mycompany.dto.NormalDTO;
+import com.mycompany.dto.PostDTO;
 import com.mycompany.dto.UsuarioDTO;
 import com.mycompany.modelo.Administrador;
+import com.mycompany.modelo.CategoriaPost;
 import com.mycompany.modelo.Estado;
 import com.mycompany.modelo.Municipio;
 import daos.IUsuarioDAO;
 import daos.UsuarioDAO;
 import fachada.Fachada;
 import fachada.IFachada;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -47,7 +50,7 @@ public class Persistencia {
         
       
         // Probar para el inicio de sesion
-//        UsuarioDTO usuarioBuscado = fachada.obtenerUsuario("madero@gmail.com", "123");
+        UsuarioDTO usuarioBuscado = fachada.obtenerUsuario("madero@gmail.com", "123");
 //        System.out.println("Usuario encontrado: " + (usuarioBuscado != null ? usuarioBuscado : "No encontrado"));
         
         
@@ -57,18 +60,18 @@ public class Persistencia {
 //        System.out.println(fachada.obtenerPostID(2L));
 //        
 //        //comentar post
-//        fachada.comentarPost(new ComentarioDTO(fecha, "afasfasf", new ComentarioDTO(), new NormalDTO("madero@gmail.com", "Madero")), fachada.obtenerPostID(5L));
+//        fachada.comentarPost(new ComentarioDTO(fecha, "afasfasf", new ComentarioDTO(), new NormalDTO("madero@gmail.com", "Madero")), fachada.obtenerPostID(6L));
 
 //        System.out.println(fachada.obtenerComentarioID(1L));
 //        
 //        //eliminar comentario
-//        fachada.eliminarComentario(new ComentarioDTO(1L, fecha, "", new NormalDTO("", "")), usuarioBuscado);
+//        fachada.eliminarComentario(new ComentarioDTO(6L, fecha, "", new NormalDTO("", "")), usuarioBuscado);
 
             //eliminar post
 //            fachada.eliminarPost(5L, usuarioBuscado);
 
             // comentar comentario
-//            fachada.responderComentario(new ComentarioDTO(fecha, "asfasf", fachada.obtenerPostID(5L), new NormalDTO("madero@gmail.com", "Madero")), fachada.obtenerComentarioID(5L));
+//            fachada.responderComentario(new ComentarioDTO(fecha, "asfasf", fachada.obtenerPostID(6L), new NormalDTO("madero@gmail.com", "Madero")), fachada.obtenerComentarioID(8L));
        
             //anclar
 //              IConexion conexion = new Conexion();
@@ -77,7 +80,25 @@ public class Persistencia {
                  
 //            fachada.anclarPost(1L, "maderito@");
 //            fachada.desanclarPost(4l);
-        
+
+                //obtener post por categoria
+//                List<PostDTO> postCategoria = new ArrayList<>();
+//               postCategoria =  fachada.obtenerPostsPorCategoria(CategoriaPost.FENOMENOS);
+//               for(PostDTO categoria: postCategoria){
+//                   if (categoria!=null) {
+//                       System.out.println(categoria);
+//                   }
+//               }
+
+                //obtener post por usuario
+//                List<PostDTO> post = new ArrayList<>();
+//                post = fachada.obtenerPostsPorUsuario("madero@gmail.com");
+//                for(PostDTO p: post){
+//                    if(p!=null){
+//                        System.out.println(p);
+//                    }
+//                }
+                
         
     }
 }
